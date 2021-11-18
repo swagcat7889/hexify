4ade18705cf51c8e7fe60f697114c69e857aa573
cola cat <86305856+swagcat7889@users.noreply.github.com>
2021-11-18 19:22:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-var readline = require('readline');
-var rl = readline.createInterface({
+'use strict'
+const readline = require('readline');
+const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
   terminal: false
 });
+const arg = process.argv[2];
 
-rl.on('line', function(line){
+if (arg == '--encode' || arg == '-e') {
+  rl.on('line', function(line){
+      process.stdout.write(Buffer.from(line, 'utf-8').toString('hex'));
+      process.stdout.write('\n');
+  });
+} else if (arg == '--decode' || arg == '-d') {
+    rl.on('line', function(line){
+      process.stdout.write(Buffer.from(line, 'hex').toString());
+      process.stdout.write('\n');
+  });
+} else {
+  rl.on('line', function(line){
     process.stdout.write(Buffer.from(line, 'utf-8').toString('hex'));
-});
+    process.stdout.write('\n');
+  });
+}
